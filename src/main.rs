23fd3eda3520cd4b9c9954d6eef09f7cdb8f9c2e
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-04-04 14:43:49 +0530

    add png headers

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,29 @@ fn main() {
     let mut buf = [0u8; 256];
     let mut jpg_headers = [0u8; 8];
     let mut jpg_end = [0u8; 2];
+    let mut png_headers = [0u8; 8];
+    let mut png_end = [0u8; 4];
+
     hex::decode_to_slice("FFD8FFe000104A46", &mut jpg_headers).unwrap();
     hex::decode_to_slice("FFD9", &mut jpg_end).unwrap();
+    hex::decode_to_slice("89504E470D0A1A0A", &mut png_headers).unwrap();
+    hex::decode_to_slice("49454E44", &mut png_end).unwrap();
+
+    let header = png_headers;
+    let end = png_end;
 
     let mut recovered_count = 0;
     while let Ok(_) = f.read_exact(&mut buf) {
-        if !buf.contains(&jpg_headers[2]) {
+        if !buf.contains(&header[2]) {
             continue;
         }
-        if let Some(pos) = find_index(&buf, &jpg_headers) {
+        if let Some(pos) = find_index_by_windowing(&buf, &header) {
             println!("pos = {}", pos);
             println!("{:X?} contains jpg headers!", buf);
             let mut fnew = File::create(format!("recovered-{}.jpg", recovered_count)).unwrap();
             fnew.write_all(&buf[pos..]).unwrap();
             while let Ok(_) = f.read_exact(&mut buf) {
-                if let Some(pos) = find_index(&buf, &jpg_end) {
+                if let Some(pos) = find_index_by_windowing(&buf, &end) {
                     fnew.write_all(&buf[..pos + 2]).unwrap();
                     println!("File recovered in {:.2?}", now.elapsed());
                     recovered_count += 1;
@@ -46,4 +54,23 @@ fn find_index(array: &[u8], sub_array: &[u8]) -> Option<usize> {
         }
     }
     None
+}
+
+fn find_index_by_windowing(array: &[u8], sub_array: &[u8]) -> Option<usize> {
+    array
+        .windows(sub_array.len())
+        .position(|data| data == sub_array)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::find_index_by_windowing;
+
+    #[test]
+    fn test_indexing() {
+        let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let sub_arr = [4, 5, 6];
+
+        assert!(find_index_by_windowing(&arr, &sub_arr) == Some(3))
+    }
 }
